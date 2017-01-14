033a17c2e83a9f4882b0bd0d742f47da8452123f
evan <wdurno@gmail.com>
2017-01-13 21:33:28 -0800

    readme touch ups

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Please see my primary portfolio website at [wdurno.squarespace.com](https://wdurno.squarespace.com).
 
 ## cuda_regression 
-This project encodes software for high-dimensional count data regression. Estimation is GPU-accelerated and interfaced via R.
+This project encodes software for high-dimensional count data regression. Estimation is **GPU-accelerated** with an **R interface**.
 
 ## nbody 
-This project encodes software for a [classic N-body simulation](https://en.wikipedia.org/wiki/N-body_simulation) in both MPI and CUDA.
+This project encodes software for a [classic N-body simulation](https://en.wikipedia.org/wiki/N-body_simulation) in both **MPI** and **CUDA**.
--- cuda_regression/user_interface.R
+++ cuda_regression/user_interface.R
@@ -4,23 +4,23 @@ source("fmwc_regression.R")
 # User interface to FMWC (Floor Model With Copula) multivariate regression tool 
 # The FMWC model is for regressing high-dimensional count data y on regressor matrix x. 
 # This software is GPU-accelerated and requires CUDA.  
-# Exact model definition is available from my thesis (not yet published!) 
-#  
-# Features to be interfaced: 
-# 1. Bootstrap significance testing 
-# 2. Approximate Hessian significance testing via Delta rule  
-# 3. Correlation network inference with and without mixed effects 
+# Exact model definition is available from my thesis (not yet published!)  
 #    
 # Features to be implemented: 
 # 1. L-BFGS optimization  
 #  
-fmwc_regression = function(y,x,formula=~.) 
+
+# Multivariate count regression estimation 
+# Count distributions are dependent through Gaussian copula with a factor model. 
+# The factor model has m factors.   
+fmwc_regression = function(y,x,formula=~.,m=3,n_bootstraps=1,CPU_threads=16,...) 
 { 
-	
+	if( n_bootstraps == 1 ) 
+	{ 
+		return( gpu_estimate(y,x,formula,m,...) ) 
+	}else{ 
+		return( get_many_bootstraps(y,x,n_threads=CPU_threads,n_bootstraps,formula,m,gpu=T,...) )  
+	} 
 } 
 
-save_bootstraps = function( bootstraps ) 
-{ 
-	
-} 
 
